45d1152eaf83d1b467d32e82d8a9b599e3b87453
Ian Maulana <ian_maulana@yahoo.com>
2019-07-16 06:50:31 +0700

    create vulator button component

---
+++ src/components/Vuelator/VuelatorBtn.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="vuelator-btn">
+    <button :class="btnClass" @click="onClick" :disabled="btnDisabled">{{btnText}}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VuelatorBtn',
+  computed: {
+    setBtnValue() {
+      const { btnValue, btnText } = this;
+      return btnValue || btnText;
+    },
+  },
+  props: {
+    btnText: {
+      type: String,
+      required: true,
+    },
+    btnClass: {
+      type: String,
+      required: true,
+    },
+    btnValue: {
+      type: String,
+      default: null,
+    },
+    btnDisabled: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  methods: {
+    onClick() {
+      const value = !this.btnValue ? this.btnText : this.btnValue;
+      this.$emit('clicked', value);
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+  .vuelator-btn {
+    display: inline-block;
+
+    .btn {
+
+      display: inline-block;
+    }
+  }
+</style>
