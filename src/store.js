98c1c85e306f8d475bda0755a4b27e310ece5faa
Ian Maulana <ian_maulana@yahoo.com>
2019-07-16 23:25:39 +0700

    Setup vuex for save logs

--- src/store.js
+++ src/store.js
@@ -5,10 +5,12 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-
+    logs: [],
   },
   mutations: {
-
+    SET_LOGS: (state, logs) => {
+      state.logs = logs;
+    },
   },
   actions: {
 
