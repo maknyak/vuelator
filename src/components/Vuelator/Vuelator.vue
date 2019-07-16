06669d938bc714251b406618a9cde152d575dcc1
Ian Maulana <ian_maulana@yahoo.com>
2019-07-16 22:16:54 +0700

    create doCalculation function

--- src/components/Vuelator/Vuelator.vue
+++ src/components/Vuelator/Vuelator.vue
@@ -22,7 +22,7 @@
     <vuelator-btn btnClass="btn" btnText="x" @clicked="setOperator"/>
     <vuelator-btn btnClass="btn" btnText=":" @clicked="setOperator"/>
     <vuelator-btn btnClass="btn" btnText="." @clicked="setDecimal"/>
-<!--    <vuelator-btn btnClass="btn" btnText="=" @clicked="doCalculate"/>-->
+    <vuelator-btn btnClass="btn" btnText="=" @clicked="doCalculate"/>
     <vuelator-btn btnClass="btn" btnText="+/-" @clicked="setNegatifNumber"/>
     <vuelator-btn btnClass="btn" btnText="%" @clicked="doPercentage"/>
     <vuelator-btn btnClass="btn" btnText="Clear" @clicked="doReset"/>
@@ -52,6 +52,7 @@ export default {
   },
   methods: {
     setDigit(value) {
+      this.total = ''; // reset total
       if (this.input === 0 || this.input === '') {
         this.input = Number(this.input + value); // is not decimal number, remove 0
       } else {
@@ -66,35 +67,77 @@ export default {
         const expression = this.expression.slice(0, -1); // remove operator
         this.expression = expression + operator; // and replace with new operator
       } else {
-        this.expression += this.input + operator; // set operator
+        if (this.total) {
+          this.expression += this.total + operator; // calculate previous total
+        } else {
+          this.expression += this.input + operator; // set operator
+        }
+
         this.input = '';
+        this.total = '';
       }
     },
     setDecimal() {
       const isDecimal = this.input.toString().includes('.');
 
+      // set value as decimal number if value contains .
       if (!isDecimal && this.input !== '') {
         this.input += '.';
       }
     },
     isOperator(tipe) {
+      // check current value is operator
       return tipe === '+' || tipe === '-' || tipe === ':' || tipe === 'x';
     },
     setNegatifNumber() {
+      // add negatif number if value more than 0
       if (this.input > 0) {
         this.input = `(-${this.input})`;
       }
     },
     doPercentage() {
       if (this.input > 0) {
+        // calculate percentage if value more than 0
         const result = Number(this.input) * 0.01;
         this.input = result;
       }
     },
     doReset() {
+      // reset all variable
       this.expression = '';
       this.total = '';
       this.input = 0;
+    },
+    doCalculate() {
+      const { isOperator } = this;
+      const last = this.expression.substr(-1);
+
+      if (isOperator(last) && this.input !== '') {
+        this.expression += this.input;
+        // convert : or x with aritmatic operator
+        this.expression = this.expression.replace(/x/g, '*');
+        // eslint-disable-next-line no-useless-escape
+        this.expression = this.expression.replace(/\:/g, '/');
+
+        // eslint-disable-next-line no-eval
+        const total = eval(this.expression); // calculate total
+        // eslint-disable-next-line no-restricted-globals
+        if (!isNaN(total)) {
+          this.total = total;
+          this.expression += `=${parseFloat(total)}`;
+
+          // log calculate history
+          if (this.expression) {
+            this.logs.push(this.expression);
+          }
+        } else {
+          alert('invalid number');
+        }
+
+        // reset variable
+        this.input = '';
+        this.expression = '';
+      }
     },
   },
 };
