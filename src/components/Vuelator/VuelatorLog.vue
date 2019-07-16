62b42710374626e9f27b86ba12cb4846a07ed89a
Ian Maulana <ian_maulana@yahoo.com>
2019-07-16 23:26:26 +0700

    Create logs component

---
+++ src/components/Vuelator/VuelatorLog.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="vuelator-log">
+    <ul v-if="logs.length > 0">
+      <li v-for="(log, index) in logs" :key="index">{{logs}}</li>
+    </ul>
+    <ul v-else>
+      <li>0</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Vuelator',
+  props: {
+    logs: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
